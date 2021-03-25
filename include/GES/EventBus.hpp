a0e333dd8c7d2e706dce2418d2123fd29ded74b8
grillow <spmr757@gmail.com>
2021-03-26 01:48:22 +0300

    unfancy

--- include/GES/EventBus.hpp
+++ include/GES/EventBus.hpp
@@ -8,8 +8,6 @@
 struct EventBus final {
 
 public:
-    EventBus(std::unique_ptr<EventBus::Impl> impl); // could not manage to make it private
-    
     static std::shared_ptr<EventBus> Create();
 
     ~EventBus();
@@ -23,6 +21,8 @@ public:
 
 private:
     struct Impl;
+public:
+    EventBus(std::unique_ptr<EventBus::Impl> impl); // could not manage to make it private
 
 private:
 
