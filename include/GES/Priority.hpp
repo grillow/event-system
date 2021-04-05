158b2c9208704e80962e3b4c06c2a1b8e9fd7e4b
grillow <spmr757@gmail.com>
2021-04-05 22:12:33 +0300

    Priorities

--- include/GES/Priority.hpp
+++ include/GES/Priority.hpp
@@ -3,12 +3,15 @@
 namespace Event {
 
     enum class Priority : int8_t {
-        HOOK     =     -2,
-        HIGH     =     -1,
-        MEDIUM   =      0,
-        LOW      =      1,
+        HOOK        =      -2,
+        VERY_HIGH   =      -1,
+        HIGH        =      -1,
+        MEDIUM      =       0,
+        LOW         =       1,
+        VERY_LOW    =       1,
+        POST        =       2,
 
-        DEFAULT  = MEDIUM
+        DEFAULT     =  MEDIUM
     };
 
 }
