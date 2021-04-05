d200130a533add9c15567067645397061e405f1f
grillow <spmr757@gmail.com>
2021-04-05 21:27:56 +0300

    Event separation

--- include/GES/Event.hpp
+++ include/GES/Event.hpp
@@ -1,35 +1,5 @@
 #pragma once
-#include <type_traits>
-#include <string_view>
-#include <functional>
-#include <cstdint>
 
-namespace Event {
-
-    // base interface
-    struct IEvent {
-        using Type_t = uint64_t;
-        virtual ~IEvent() = default;
-        virtual Type_t Type() const = 0;
-    };
-
-
-    template <typename T>
-    concept EventDerived = std::is_base_of<IEvent, T>::value;
-
-
-    // tools
-    /*constexpr*/ inline IEvent::Type_t stot (std::string_view eventName) {
-        return std::hash<std::string_view>{}(eventName);
-    }
-
-    template <typename T>
-    struct EventTemplate : IEvent {
-        static inline const Type_t ID = stot( typeid(T).name() );
-        Type_t Type() const override final {
-            return ID;
-        }
-    };
-
-}
+#include "Event/IEvent.hpp"
+#include "Event/EventTemplate.hpp"
 
---
+++ include/GES/Event/EventTemplate.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "IEvent.hpp"
+
+#include <string_view>
+#include <functional>
+
+namespace Event {
+
+    /*constexpr*/ inline IEvent::Type_t stot (std::string_view eventName) {
+        return std::hash<std::string_view>{}(eventName);
+    }
+
+    template <typename T>
+    struct EventTemplate : IEvent {
+        static inline const Type_t ID = stot( typeid(T).name() );
+        Type_t Type() const override final {
+            return ID;
+        }
+    };
+
+}
+
